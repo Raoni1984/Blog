ddf19737d038dee8ace1717b3a545dd4eb9219a3
Raoni1984 <raonico@gmail.com>
2017-03-22 01:53:45 -0300

    Adicionei o Form Cadastro Usuario e botoes na Pagina home

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -1,2 +1,33 @@
 class UsuariosController < ApplicationController
+  def new
+    @usuario = Usuario.new
+  end
+
+  def create
+    @usuario = Usuario.new(params[user_params])
+    if @usuario.save
+      redirect_to new_usuario_path
+    end
+  end
+
+  def user_params
+    params.require(:usuario).permit(:nome, :sobrenome, :email, :empresa, :datahora)
+  end
+
+  def index
+    @usuarios = Usuario.all
+    # Patching all Listing
+
+    @usuario = Usuario.where(id: params[:id]) if params[:id].present?
+    # Find By Id (For pagination, the 'where' statement result is Listing ActiveRecord::Relationship )
+    @usuarios = @usuarios.search(params[:search]) if params[:search].present?
+    # Search using Keyword
+    @usuarios = @usuarios.paginate(:page => params[:page], :per_page => 10)
+    # Pagination
+    @usuario = Usuario.find_by_name(params[:name]) if params[:name].present?
+    # # Find User using name column
+    # @categories = Category.all
+
+  end
+
 end
